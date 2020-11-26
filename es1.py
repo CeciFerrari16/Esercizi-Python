2fe2dd1f664237656bf81d65a91e53700029e1f4
CeciFerrari16 <cecilia.ferrari2@liceoariostospallanzani-re.edu.it>
2020-11-26 10:21:51 +0100

    Aggiungo il codice per stampare il vincitore

--- es1.py
+++ es1.py
@@ -6,9 +6,12 @@ tot = a + b
 perc1 = round(100 * a / tot)
 perc2 = round(100 * b / tot) 
 
+print("La percentuale del primo candidato è:", perc1, "%")
+print("La percentuale del secondo candidato è:", perc2, "%")
+
 if a == b:
     print("Il risultato delle elezioni è un pareggio")
-    print(perc1, perc2)
+elif perc1 > perc2:
+    print("Il vincitore è il primo candidato")
 else:
-    print("La percentuale del primo candidato è:", perc1, "%")
-    print("La percentuale del secondo candidato è:", perc2, "%")
+    print("Il vincitore è il secondo candidato")
